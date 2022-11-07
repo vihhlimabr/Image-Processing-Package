e945baa20a1bcd80a62b845910c8b632129b32b0
vihhlimabr <vihhlima@hotmail.com>
2022-11-07 13:35:10 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processsing",
+    version="0.0.1",
+    author="Vitor Lima",
+    author_email="vihhlima@hotmail.com",
+    description="Image processing package",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
