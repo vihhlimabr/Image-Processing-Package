4b508fc05b58a90bad1076e68abdfcdf56c31ec3
vihhlimabr <vihhlima@hotmail.com>
2022-11-07 13:38:57 -0300

    url added to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     description="Image processing package",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/vihhlimabr/Image-Processing-Package.git"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
