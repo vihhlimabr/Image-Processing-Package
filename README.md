09406acd9828e24ca9287416f01747bd50478d68
vihhlimabr <vihhlima@hotmail.com>
2022-11-07 14:15:14 -0300

    acertando pacote com io

--- README.md
+++ README.md
@@ -1,31 +1,26 @@
-# package_name
+# vihhlima_image_processing
 
 Description. 
 The package package_name is used to:
-	Processing 
-		- Histogram matching
-		- Structural similarity
-		- Resize image
+
 	Utils:
 		- Read image
 		- Save image
-		- Plot image
-		- Plot result
-		- Plot histogram
+
 
 ## Installation
 
 Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
 
 ```bash
-pip install package_name
+pip install vihhlima_image_processing
 ```
 
 ## Usage
 
 ```python
-from package_name.module1_name import file1_name
-file1_name.my_function()
+from vihhlima_image_processing.utils import io
+io.read_image(path)
 ```
 
 ## Author
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as f:
     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+#with open("requirements.txt") as f:
+ #   requirements = f.read().splitlines()
 
 setup(
-    name="image_processsing",
+    name="vihhlima_image_processsing",
     version="0.0.1",
     author="Vitor Lima",
     author_email="vihhlima@hotmail.com",
@@ -16,6 +16,6 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/vihhlimabr/Image-Processing-Package.git"
     packages=find_packages(),
-    install_requires=requirements,
+    #install_requires=requirements,
     python_requires='>=3.8',
 )
